a917a73061e3b0725388da07e33e2513dde43c67
moha-mame <moha.mame04@gmail.com>
2023-07-06 10:03:36 +0000

    lockboxes#

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    if n == 0:
+        return False
+
+    # Set to store the keys we have collected
+    keys = set([0])
+
+    # Queue to store the boxes we can currently open
+    queue = deque([0])
+
+    while queue:
+        box = queue.popleft()
+        for key in boxes[box]:
+            if key not in keys and key < n:
+                keys.add(key)
+                queue.append(key)
+
+    return len(keys) == n
