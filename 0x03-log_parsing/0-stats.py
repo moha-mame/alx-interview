9e68e621510051a20e93e5cd105e06b67f5ad3e8
mohammed Abate <71933125+moha-mame@users.noreply.github.com>
2023-07-19 14:07:50 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,34 +12,37 @@ def print_statistics(status_codes, total_file_size):
     Returns:
         None
     """
-    print("File size:", total_file_size)
-    for code, count in sorted(status_codes.items()):
-        if count != 0:
-            print(f"{code}: {count}")
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
 
-def main():
-    total_file_size = 0
-    counter = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
+total_file_size = 0
+code = 0
+counter = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+  
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()
+        parsed_line = parsed_line[::-1]
 
-    try:
-        for line in sys.stdin:
-            parsed_line = line.split()[::-1]  # Split and reverse the line
+        if len(parsed_line) > 2:
+            counter += 1
 
-            if len(parsed_line) > 2:
-                counter += 1
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])
+                code = parsed_line[1]
 
-                if counter <= 10:
-                    total_file_size += int(parsed_line[0])  # file size
-                    code = parsed_line[1]  # status code
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-                    if (code in status_codes.keys()):
-                        status_codes[code] += 1
+            if (counter == 10):
+                print_msg(status_codes, total_file_size)
+                counter = 0
 
-                if counter == 10:
-                    print_statistics(status_codes, total_file_size)
-                    counter = 0
-    finally:
-        print_statistics(status_codes, total_file_size)
+finally:
+    print_statistics(status_codes, total_file_size)      
