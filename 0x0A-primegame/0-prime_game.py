2040f9b2e88089d6c55915ec1fd4be20f83ae114
mohammed Abate <71933125+moha-mame@users.noreply.github.com>
2023-09-07 17:20:03 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,50 +2,117 @@
 """ Prime Game """
 
 
-def isprime(n):
-    """ Return prime number """
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
-def delete_numbers(n, nums):
-    """ Remove numbers - return zero """
-    for i in range(len(nums)):
-        if nums[i] % n == 0:
-            nums[i] = 0
-
-
 def isWinner(x, nums):
-    """ Return name of player that won
-    most rounds
-    """
-    nums.sort()
-    winner = False
-    Maria = 0
-    Ben = 0
-    for game in range(x):
-        nums2 = list(range(1, nums[game] + 1))
-        # print("nums: ", nums2)
-        turn = 0
-        while True:
-            change = False
-            for i, n in enumerate(nums2):
-                # print("n: ", n, "i: ", i)
-                if n > 1 and isprime(n):
-                    delete_numbers(n, nums2)
-                    change = True
-                    turn += 1
-                    break
-            if change is False:
-                break
-        if turn % 2 != 0:
-            Maria += 1
-        else:
-            Ben += 1
-    if Maria == Ben:
-        return None
-    if Maria > Ben:
-        return "Maria"
+  """
+  Determines the winner of the prime game.
+
+  Args:
+    x: The number of rounds.
+    nums: An array of the numbers in each round.
+
+  Returns:
+    The name of the player that won the most rounds, or None if the winner cannot be determined.
+  """
+
+  scores = {"Maria": 0, "Ben": 0}
+  for i in range(x):
+    current_nums = nums[i]
+    winner = findWinner(current_nums)
+    scores[winner] += 1
+
+  most_wins = max(scores.values())
+  winners = [key for key, value in scores.items() if value == most_wins]
+
+  if len(winners) == 1:
+    return winners[0]
+  else:
+    return None
+
+def findWinner(nums):
+  """
+  Determines the winner of a single round of the prime game.
+
+  Args:
+    nums: The numbers in the round.
+
+  Returns:
+    The name of the player that won the round, or None if the winner cannot be determined.
+  """
+
+  if len(nums) == 1:
     return "Ben"
+
+  primes = [prime for prime in nums if isPrime(prime)]
+  if len(primes) == 0:
+    return "Maria"
+
+  next_move = findBestMove(primes)
+  if next_move is None:
+    return "Maria"
+
+  return "Ben"
+
+def isPrime(num):
+  """
+  Determines if a number is prime.
+
+  Args:
+    num: The number to check.
+
+  Returns:
+    True if the number is prime, False otherwise.
+  """
+
+  if num < 2:
+    return False
+
+  for i in range(2, int(num ** 0.5) + 1):
+    if num % i == 0:
+      return False
+
+  return True
+
+def findBestMove(primes):
+  """
+  Finds the best move for a player in a round of the prime game.
+
+  Args:
+    primes: The prime numbers in the round.
+
+  Returns:
+    The index of the best move, or None if there is no best move.
+  """
+
+  best_move = None
+  best_score = -float("inf")
+
+  for i in range(len(primes)):
+    new_primes = primes[:i] + primes[i + 1:]
+    score = getScore(new_primes)
+
+    if score > best_score:
+      best_move = i
+      best_score = score
+
+  return best_move
+
+def getScore(primes):
+  """
+  Gets the score of a set of prime numbers.
+
+  Args:
+    primes: The prime numbers to get the score for.
+
+  Returns:
+    The score of the set of prime numbers.
+  """
+
+  score = len(primes)
+
+  for prime in primes:
+    for i in range(prime, len(primes)):
+      if primes[i] % prime == 0:
+        score -= 1
+        break
+
+  return score
